3cb8ecb28210bd24aea4b43cbdcf07dad0b516eb
Dave Pitts <dgapitts@gmail.com>
2023-07-27 18:29:10 +0200

    hackerrank challenge 006 leap year func

--- code/hackerrank-006-leap-year-func.py
+++ code/hackerrank-006-leap-year-func.py
@@ -15,3 +15,17 @@ def is_leap(year):
 
     return leap
 ˝
+
+print(is_leap(1500))
+print(is_leap(1600))
+print(is_leap(1700))
+print(is_leap(1800))
+print(is_leap(1900))
+print(is_leap(2000))
+print(is_leap(2001))
+print(is_leap(2002))
+print(is_leap(2003))
+print(is_leap(2004))
+print(is_leap(2005))
+print(is_leap(2006))
+print(is_leap(2007))
